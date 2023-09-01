b3ad375960544313d4332093aaf723a2f0c3154f
danieletulone <danieletulone.work@gmail.com>
2023-09-01 13:59:08 +0200

    update PasswordUpdateRequest: - implements interface - add passwordRules.

--- src/Http/Requests/PasswordUpdateRequest.php
+++ src/Http/Requests/PasswordUpdateRequest.php
@@ -2,18 +2,27 @@
 
 namespace Dedecube\Profile\Http\Requests;
 
+use Dedecube\Profile\Contracts\PasswordUpdateRequestInterface;
 use Illuminate\Foundation\Http\FormRequest;
 
-class PasswordUpdateRequest extends FormRequest
+class PasswordUpdateRequest extends FormRequest implements PasswordUpdateRequestInterface
 {
     /**
      * Get the validation rules that apply to the request.
      */
     public function rules(): array
     {
+
         return [
             'current' => 'required|hash_match:'.auth()->user()->password,
-            'new' => 'required|confirmed|min:6',
+            'new' => array_merge(['required', 'confirmed'], $this->passwordRules()),
+        ];
+    }
+
+    public function passwordRules(): array
+    {
+        return [
+            'min:6',
         ];
     }
 }
